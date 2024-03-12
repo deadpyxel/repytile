4428018a093817971b7b13134c38d6b6f4bd0549
Robson Cruz <ghost0092@gmail.com>
2024-03-12 14:10:24 -0300

    feat(inline_elements.py): add TextNode class
    TextNode will be responsible for dealing with the inline elements of
    Markdown documents

---
+++ repytile/inline_elements.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+
+class TextNode:
+    def __init__(self, text: str, text_type: str, url: Optional[str] = None) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, TextNode):
+            return False
+        return (
+            self.text == __value.text
+            and self.text_type == __value.text_type
+            and self.url == __value.url
+        )
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
