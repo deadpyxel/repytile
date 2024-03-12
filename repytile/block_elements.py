898b8f66b3d7fe91c6803dce411a8a5aea25405e
Robson Cruz <ghost0092@gmail.com>
2024-03-12 15:13:52 -0300

    feat(block_elements.py): add HTMLNode class
    This class will be responsible for dealing with block elements like divs

---
+++ repytile/block_elements.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from typing import Optional
+
+
+class HTMLNode:
+    def __init__(
+        self,
+        tag: Optional[str] = None,
+        value: Optional[str] = None,
+        children: Optional[list[HTMLNode]] = None,
+        props: Optional[dict[str, str]] = None,
+    ) -> None:
+        """
+        Initialize an HTMLNode object with the specified tag, value, children, and properties.
+
+        Parameters:
+            tag (str): The HTML tag for the node. If not provided, the node will render as raw text.
+            value (str): The value/content of the node. If not provided, the node will be assumed to have children.
+            children (list[HTMLNode]): A list of child HTMLNode objects. If not provided, the node will be assumed to have a value.
+            props (dict[str, str]): A dictionary of properties/attributes for the node. If not provided, the node will have no attributes.
+
+        """
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self) -> str:
+        raise NotImplementedError
+
+    def props_to_html(self) -> str:
+        """
+        Converts the properties of an object into HTML attribute format.
+
+        This method takes the properties of an object stored in a dictionary format and converts them into a string of HTML attributes.
+        Each key-value pair in the dictionary is converted into a string in the format 'key="value"'.
+        If the object has no properties, an empty string is returned.
+
+        Returns:
+            str: A string containing the HTML attributes generated from the object's properties.
+
+        Example:
+            >>> obj = {'id': 'my_id', 'class': 'my_class', 'style': 'color: red;'}
+            >>> obj.props_to_html()
+            'id="my_id" class="my_class" style="color: red;"'
+        """
+        if not self.props:
+            return ""
+        html_props = []
+        for attr, val in self.props.items():
+            html_props.append(f'{attr}="{val}"')
+
+        return " ".join(html_props)
+
+    def __repr__(self) -> str:
+        return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
---
+++ tests/test_blocknode.py
@@ -0,0 +1,52 @@
+import pytest
+
+from repytile.block_elements import HTMLNode
+
+
+def test_convertion_from_props_to_html_matches_expectation() -> None:
+    block_node = HTMLNode(props={"href": "https://www.example.com", "target": "_blank"})
+    expected_html_props = 'href="https://www.example.com" target="_blank"'
+
+    assert block_node.props_to_html() == expected_html_props
+
+
+def test_when_props_is_empty_empty_html_is_returned() -> None:
+    block_node = HTMLNode()
+
+    assert block_node.props_to_html() == ""
+
+
+@pytest.mark.parametrize(
+    "html_node,expected",
+    [
+        pytest.param(
+            HTMLNode(tag="p"),
+            "HTMLNode(tag=p, value=None, children=None, props=None)",
+            id="tag_only",
+        ),
+        pytest.param(
+            HTMLNode(value="hello"),
+            "HTMLNode(tag=None, value=hello, children=None, props=None)",
+            id="value_only",
+        ),
+        pytest.param(
+            HTMLNode(children=[HTMLNode()]),
+            "HTMLNode(tag=None, value=None, children=[HTMLNode(tag=None, value=None, children=None, props=None)], props=None)",
+            id="children_with_none_fields",
+        ),
+        pytest.param(
+            HTMLNode(children=[HTMLNode(value="p")]),
+            "HTMLNode(tag=None, value=None, children=[HTMLNode(tag=None, value=p, children=None, props=None)], props=None)",
+            id="children_with_fields",
+        ),
+        pytest.param(
+            HTMLNode(props={"href": "link"}),
+            "HTMLNode(tag=None, value=None, children=None, props={'href': 'link'})",
+            id="props_only",
+        ),
+    ],
+)
+def test_htmlnode_repr_follows_expected_format(
+    html_node: HTMLNode, expected: str
+) -> None:
+    assert str(html_node) == expected
