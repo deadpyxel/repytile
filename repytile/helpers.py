e47d498265e520d88731f21eedb04d89ab96e2ef
Robson Cruz <ghost0092@gmail.com>
2024-03-12 19:56:11 -0300

    feat(helpers.py): add function to convert TextNode to LeafNode

---
+++ repytile/helpers.py
@@ -0,0 +1,58 @@
+from repytile.block_elements import LeafNode
+from repytile.exceptions import InvalidElementType
+from repytile.inline_elements import TextNode
+
+
+ALLOWED_TYPES = ("text", "bold", "italic", "code", "link", "image")
+
+
+def text_node_to_html_node(tn: TextNode) -> LeafNode:
+    """
+    Converts a TextNode object to a LeafNode object for HTML representation.
+
+    Arguments:
+        tn (TextNode): The TextNode object to be converted.
+
+    Returns:
+        LeafNode: The converted LeafNode object for HTML representation.
+
+    Raises:
+        InvalidElementType: If the text_type of the TextNode is not in the ALLOWED_TYPES list.
+
+    The function checks if the text_type of the TextNode is allowed for conversion. If it is not in the ALLOWED_TYPES
+    tuple, it raises an InvalidElementType exception. Otherwise, it converts the TextNode to a LeafNode object with
+    appropriate tag and properties for HTML representation.
+
+    The mapping of text_type to HTML tag is as follows:
+    - "text": No tag, just the text value
+    - "bold": <b> tag
+    - "italic": <i> tag
+    - "code": <code> tag
+    - "link": <a> tag with href property
+    - "image": <img> tag with src and alt properties
+    """
+
+    if tn.text_type not in ALLOWED_TYPES:
+        raise InvalidElementType(
+            f"The type of TextNode {tn.text_type} is not allowed for conversion"
+        )
+    if tn.text_type == "text":
+        return LeafNode(value=tn.text)
+
+    tag_mapping = {
+        "bold": "b",
+        "italic": "i",
+        "code": "code",
+        "link": "a",
+        "image": "img",
+    }
+
+    tag = tag_mapping[tn.text_type]
+    props = {}
+    if tn.text_type == "link":
+        props["href"] = tn.url
+    elif tn.text_type == "image":
+        props["src"] = tn.url
+        props["alt"] = tn.text
+
+    return LeafNode(tag=tag, value=tn.text, props=props)
---
+++ tests/test_helpers.py
@@ -0,0 +1,72 @@
+import pytest
+from repytile.block_elements import LeafNode
+
+from repytile.exceptions import InvalidElementType
+from repytile.helpers import text_node_to_html_node
+from repytile.inline_elements import TextNode
+
+
+def test_conversion_from_text_node_to_html_node_raise_error_if_type_not_allowed() -> (
+    None
+):
+    with pytest.raises(InvalidElementType):
+        text_node_to_html_node(TextNode(text="test", text_type="none"))
+
+
+def test_text_node_to_html_node_raw_text():
+    tn = TextNode(text="Hello World", text_type="text")
+    result = text_node_to_html_node(tn)
+
+    assert result.tag is None
+    assert result.value == "Hello World"
+    assert result.props == None
+
+
+def test_text_node_to_html_node_bold_text():
+    tn = TextNode(text="Bold Text", text_type="bold")
+    result = text_node_to_html_node(tn)
+
+    assert result.tag == "b"
+    assert result.value == "Bold Text"
+    assert result.props == {}
+
+
+def test_text_node_to_html_italic_text():
+    tn = TextNode(text="Italic Text", text_type="italic")
+    result = text_node_to_html_node(tn)
+
+    assert result.tag == "i"
+    assert result.value == "Italic Text"
+    assert result.props == {}
+
+
+def test_text_node_to_html_code_block():
+    tn = TextNode(text="Code Block", text_type="code")
+    result = text_node_to_html_node(tn)
+
+    assert result.tag == "code"
+    assert result.value == "Code Block"
+    assert result.props == {}
+
+
+def test_text_node_to_html_node_link():
+    tn = TextNode(text="Click Here", text_type="link", url="https://example.com")
+    result = text_node_to_html_node(tn)
+
+    assert result.tag == "a"
+    assert result.value == "Click Here"
+    assert result.props == {"href": "https://example.com"}
+
+
+def test_text_node_to_html_image():
+    tn = TextNode(
+        text="Description", text_type="image", url="https://www.example.com/image.png"
+    )
+    result = text_node_to_html_node(tn)
+
+    assert result.tag == "img"
+    assert result.value == "Description"
+    assert result.props == {
+        "src": "https://www.example.com/image.png",
+        "alt": "Description",
+    }
