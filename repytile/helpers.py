5e23b5c14e3e5a35b551329edfd98fd197cae551
Robson Cruz <ghost0092@gmail.com>
2024-03-15 18:13:24 -0300

    wip: development of TextNode splitting solution

--- repytile/helpers.py
+++ repytile/helpers.py
@@ -1,4 +1,6 @@
-from repytile.block_elements import LeafNode
+import re
+
+from repytile.block_elements import HTMLNode, LeafNode
 from repytile.exceptions import InvalidElementType
 from repytile.inline_elements import TextNode
 
@@ -54,3 +56,48 @@ def text_node_to_html_node(tn: TextNode) -> LeafNode:
         props["alt"] = tn.text
 
     return LeafNode(tag=tag, value=tn.text, props=props)
+
+
+def _split_keep(input_str: str, sep: str) -> list[str]:
+    """
+    Split a string based on a separator while keeping the separator as part of the result.
+
+    Arguments:
+        input_str (str): The input string to be split.
+        sep (str): The separator to split the input string on.
+
+    Returns:
+        list[str]: A list of strings resulting from splitting the input string based on the separator,
+                   with the separator included in each split part.
+
+    Example:
+        >>> _split_keep("apple,orange,banana", ",")
+        ['apple', ',orange,', 'banana']
+    """
+    # Escape any special characters on separator
+    sep = re.escape(sep)
+    # Define the regex for extraction, format is sep(words)sep, as separators always should appear in pairs
+    # We use parenthesis to keep the separator as part of the capturing group
+    expr = rf"({sep}[^*]+{sep})"
+    # split the input string into parts using the separator
+    result = re.split(expr, input_str)
+    # remove any empty strings
+    result = [split_part for split_part in result if split_part]
+    return result
+
+
+def split_nodes_delimiter(
+    old_nodes: list[TextNode | HTMLNode], delimiter: str, text_type: str
+) -> list[TextNode | HTMLNode]:
+    allowed_node_types = [TAG_MAPPING.keys()] + ["text"]
+    resulting_nodes = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode):
+            resulting_nodes.append(node)
+            continue
+        if node.text_type not in allowed_node_types:
+            raise InvalidElementType(
+                f"The type of TextNode {node.text_type} is not processable."
+            )
+
+    return resulting_nodes
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,7 +1,7 @@
 import pytest
 
 from repytile.exceptions import InvalidElementType
-from repytile.helpers import text_node_to_html_node
+from repytile.helpers import _split_keep, split_nodes_delimiter, text_node_to_html_node
 from repytile.inline_elements import TextNode
 
 
@@ -67,3 +67,137 @@ def test_textnode_to_htmlnode_image():
         "src": "https://www.example.com/image.png",
         "alt": "Description",
     }
+
+
+def test_split_textnodes_raises_exception_on_invalid_textnode_type() -> None:
+    with pytest.raises(InvalidElementType):
+        split_nodes_delimiter(
+            old_nodes=[TextNode("test", text_type="none")],
+            delimiter=".",
+            text_type="type",
+        )
+
+
+@pytest.mark.parametrize(
+    "input_str,sep,expected",
+    [
+        pytest.param(
+            "This has **bold** text",
+            "**",
+            ["This has ", "**bold**", " text"],
+            id="bold-text",
+        ),
+        pytest.param(
+            "apple*banana*cherry",
+            "*",
+            ["apple", "*banana*", "cherry"],
+            id="italic-text",
+        ),
+        pytest.param(
+            "`code` block",
+            "`",
+            ["`code`", " block"],
+            id="code-block",
+        ),
+    ],
+)
+def test_split_with_delimiters_keeps_separator(
+    input_str: str, sep: str, expected: list[str]
+) -> None:
+    assert _split_keep(input_str, sep) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,sep,expected",
+    [
+        pytest.param(
+            "This has **bold text",
+            "**",
+            ["This has **bold text"],
+            id="bold-sep",
+        ),
+        pytest.param(
+            "apple*banana cherry",
+            "*",
+            ["apple*banana cherry"],
+            id="italic-sep",
+        ),
+        pytest.param(
+            "code` block",
+            "`",
+            ["code` block"],
+            id="code-sep",
+        ),
+    ],
+)
+def test_split_with_delimiters_only_splits_when_separator_pairs_exist(
+    input_str: str, sep: str, expected: list[str]
+) -> None:
+    result = _split_keep(input_str, sep)
+    assert len(result) == 1
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,sep,expected",
+    [
+        pytest.param(
+            "This has **bold** text and *italic*",
+            "**",
+            ["This has ", "**bold**", " text and *italic*"],
+            id="bold-text-with-italic-after",
+        ),
+        pytest.param(
+            "This has **bold with *italic* text**",
+            "**",
+            ["This has ", "**bold**", " text and *italic*"],
+            id="bold-text-with-italic-inside",
+        ),
+    ],
+)
+def test_split_with_multiple_text_types_only_handles_passed_separator(
+    input_str: str, sep: str, expected: list[str]
+) -> None:
+    assert _split_keep(input_str, sep) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,sep,expected",
+    [
+        pytest.param(
+            "This has **bold** text and more **bolder** text",
+            "**",
+            ["This has ", "**bold**", " text and more ", "**bolder**", " text"],
+            id="multiple-bold-text",
+        ),
+        pytest.param(
+            "This has **bold** **bolder** text",
+            "**",
+            ["This has ", "**bold**", " ", "**bolder**", " text"],
+            id="consecultive-bold-text",
+        ),
+    ],
+)
+def test_split_with_multiple_occurences(
+    input_str: str, sep: str, expected: list[str]
+) -> None:
+    assert _split_keep(input_str, sep) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,sep,expected",
+    [
+        pytest.param(
+            "This has **bold* text is wrong",
+            "**",
+            [
+                "This has **bold* text is wrong",
+            ],
+            id="bold-text-with-mismatching-sep",
+        ),
+    ],
+)
+def test_split_with_mismatching_separators_keeps_string_unchanged(
+    input_str: str, sep: str, expected: list[str]
+) -> None:
+    assert _split_keep(input_str, sep) == expected
